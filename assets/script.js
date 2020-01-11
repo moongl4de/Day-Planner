045de12d3fc6ea67ac53bf8950097d70cc58cc62
alexgignilliat <56325924+alexgignilliat@users.noreply.github.com>
2020-01-10 19:29:16 -0500

    Final major content push. Lesson learned: stop overthinking. Keep it simple.

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,5 @@
+//displays the date and time at the top of the application
 $("#top-text").text(moment().format("dddd, MMMM Do YYYY -  h:mm a"))
-
-
-
 
 var dayPlanner = {
     input1: "",
@@ -15,64 +13,41 @@ var dayPlanner = {
     input9: "",
     input10: "",
 }
-
+//using 24 hour time because it ended up being way easier than trying to determine if 9am is earlier or later than 1pm lol
 var timetest= parseInt(moment().format("k"))
 console.log(timetest)
+var timeBlock = $(".timeClass");
+var timeBlockText = $(".inputfield")
+console.log("timeblock length: " + timeBlock.length)
+//a for loop that loops thru each element with a class of ".timeClass", grabs the "data-time" attribute and then checks to see if its greater than/less than/equal to the actual time, then gives it the appropriate class for past, present or future
+for(i = 0; i < timeBlock.length; i++){
+    var dataTime = parseInt(timeBlock[i].getAttribute("data-time"));
+    console.log(dataTime)
+
+    if (timetest > dataTime) {
+        timeBlock[i].classList.add('past');
+        timeBlockText[i].classList.add("past")
+        console.log("past: " + dataTime)
+    }
+    if (timetest === dataTime) {
+        timeBlock[i].classList.add('present');
+        timeBlockText[i].classList.add("present")
+        console.log("present: " + dataTime)
+    }
+    if (timetest < dataTime) {
+        timeBlock[i].classList.add('future');
+        timeBlockText[i].classList.add("future")
+        console.log("future: " + dataTime)
+    }
 
 
-
-
-
-// timeColor()
-// function timeColor(){
-    
-    
-//     var divClass = $(".timeClass")
-//     for(i = 9; i < 18; i++){
-//     var dataTime = divClass.attr("data-time-" + i)
-//         console.log(dataTime)
-     
-        
-         
-//     }
-
-$('.timeClass').each(function(i, obj) {
-    
-});
-
-    
-
-// }
-function rowColors(){
-    if (
-        $(".timeClass").attr("data-time") > timetest) {
-            $(".timeClass").addClass("future");
-        }
-    else if (
-        $(".timeClass").attr("data-time") === timetest) {
-            $(".timeClass").addClass("present");
-        }
-    else (
-        $(".timeClass").attr("data-time") < timetest); {
-            $(".timeClass").add("past");
-        }   
-
-};
-
-rowColors();
-
-
-
-
-// if(momentTime.isBefore(laterMomentTime)){
-//   console.log("Yes 1:30 pm is earlier");
-// }
+}
 
 function loadData(){
     for(var i = 1; i < 11; i++){
        var getData = localStorage.getItem("input" + i)
        var parsedData = JSON.parse(getData);
-    //    console.log("parsedData " + i + ": " + parsedData)
+    console.log("parsedData " + i + ": " + parsedData)
        $("#input" + i).val(parsedData);
     }
 }
@@ -91,7 +66,7 @@ $(".planner-buttons").on('click', function(){
 
 
 loadData();
-// timeColor();
+
 
 
 
